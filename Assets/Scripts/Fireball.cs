639cc254f10dddb5dca284d3bf397892811722ff
Utkan <utkancelik@hacettepe.edu.tr>
2022-12-13 13:30:13 +0300

    Fireballs

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public Vector2 StartingVelocity;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = StartingVelocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Enemy>(out var enemy))
+            enemy.Die();
+    }
+}
--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,24 @@ public class MoveLeft : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f;
 
+    private Enemy enemy;
+
+    private void Awake()
+    {
+        enemy = GetComponent<Enemy>();
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
+        transform.Translate(speed * Time.deltaTime * Vector3.left, Space.World);
 
         if (transform.position.x < -15.0f)
         {
             transform.position += new Vector3(30, 0, 0);
-
             ShowRandomSprite();
+
+            if(enemy != null)
+                enemy.Respawn();
         }
             
     }
