ca6202198f2e999cf42712ad3c3aae8d5f3ccf4a
Utkan <utkancelik@hacettepe.edu.tr>
2022-12-13 10:06:26 +0300

    Make the tree a prefab and make it spawn with random sprite if it was out of map.

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -1,14 +1,39 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f;
 
+
     void Update()
     {
-          transform.Translate(Vector3.left * Time.deltaTime * speed);
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
 
         if (transform.position.x < -15.0f)
+        {
             transform.position += new Vector3(30, 0, 0);
+
+            ShowRandomSprite();
+        }
+            
     }
+    private void OnEnable()
+    {
+        ShowRandomSprite();
+    }
+
+    private void ShowRandomSprite()
+    {
+        int randIdx = Random.Range(0, 4);
+        int childCount = transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            child.gameObject.SetActive(randIdx == i);
+        }
+    }
+
+    
 }
